b478a3818aab220536c3dd1d98fc22d65a3eb575
Sanjit Joseph <sjosephk1975@gmail.com>
2024-05-19 11:05:37 +0100

    First commit

---
+++ lib/string_enhancer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "string_enhancer/version"
+
+module StringEnhancer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/string_enhancer/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module StringEnhancer
+  VERSION = "0.1.0"
+end
